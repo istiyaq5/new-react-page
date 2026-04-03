43221d85b5e9f4bfed927b1ede5c3ee0e6bae0c3
istiyaq5 <istiyaqspc@gmail.com>
2026-04-04 03:55:07 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Home from './components/home/home'
+
+const App = () => {
+  return (
+    <div>
+    <Home></Home>  
+    
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/home/home.jsx
@@ -0,0 +1,9 @@
+function Home() {
+  return (
+    <div>
+      <hl> I am from Home Component </hl>
+      <h1>hello mf</h1>
+    </div>
+  );
+}
+export default Home;
